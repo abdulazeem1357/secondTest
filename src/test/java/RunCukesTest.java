193ff414c69a8393af00f61aba00d65acefda002
Azeem Baloch <abdulazeem1357@gmail.com>
2021-11-28 16:23:55 +0500

    monochrome = true

--- src/test/java/RunCukesTest.java
+++ src/test/java/RunCukesTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources"},
+        monochrome = true,
         plugin = {"pretty", "html:target/html-test-output.html",
                 "json:target/cucumber.json",
                 "junit:target/cucumber.xml" }
